22cf43cfd5e79d22034a17dc6202190cfac818e5
ANesterenko <makarohi@bk.ru>
2025-01-15 17:58:25 +0200

    типы даннных

--- 15-01-25/lastan1.py
+++ 15-01-25/lastan1.py
@@ -1,2 +1,9 @@
-print("hellow world")
-print("buy")
+var_int = 4  # целое число
+var_float = 8.4 / 2  # вещественное число
+var_str = "行書"  # строка
+var_list = ["fk", 42, 5.9]  # список
+var_tuple = (81281991291971 ** 213132132, "832",)  # кортеж
+var_set = {22, 22, 22, 2, 2, 2, 2}  # множество
+var_dict = {"name": "Adolf", "age":19, "funny":False}#cловарь
+var_bool = True# логика
+print(var_tuple)
